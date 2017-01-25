5155a48c23efbee53b4722138604f294ffa7debd
Stephan Otte <stephan.otte@web.de>
2017-01-25 22:50:17 +0100

    Initial commit

---
+++ app.component.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by stephan on 25.01.17.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'ponyracer-app',
+    template: '<h1>PonyRacer</h1>'
+})
+export class PonyRacerAppComponent {
+
+}
---
+++ main.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by stephan on 25.01.17.
+ */
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
+
