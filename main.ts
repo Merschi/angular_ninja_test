225c0cca5824b82e6619ec3f66f357f01ca6eda5
Stephan Otte <stephan.otte@web.de>
2017-01-27 21:47:11 +0100

    ttt

--- main.ts
+++ main.ts
@@ -5,4 +5,5 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule);
+yxvyxcvyxvyxvy
 
